0ab5aa7cf318e2b7b05eb530e7c932a6a46a4c5e
INVICTUSHO7E <106069401+INVICTUSHO7E@users.noreply.github.com>
2023-09-09 09:44:10 +0100

    Add files via upload

---
+++ First HNG task/script.js
@@ -0,0 +1,16 @@
+document.getElementById("slackUserName").innerHTML = 'Isola Oluwagbohunmi'
+
+
+
+ document.getElementById("currentDayOfTheWeek").innerHTML = new Date()
+ document.getElementById("myTrack").innerHTML = 'Frontend'
+ document.getElementById("currentUTCTime").innerHTML = toUTCString()
+
+
+
+// import { render, screen } from '@testing-library/react';
+
+// render(<MyComponent />);
+
+// const button = screen.getByTestId('my-button');
+// const input = screen.getByTestId('username-input');
